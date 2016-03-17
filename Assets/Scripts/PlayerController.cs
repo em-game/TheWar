8ff5df19c038b9a1e66ee2d3eb0a53e1d377e5e2
orangeem <orangeem@naver.com>
2016-03-17 02:03:36 -0400

    change terrian size, add gun and reticule

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//PUBLIC MEMBER VARIABLES
+	public Transform flashPoint;
+	public GameObject muzzleFlash;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Instantiate (this.muzzleFlash, flashPoint.position, Quaternion.identity);
+		}
+	}
+}
